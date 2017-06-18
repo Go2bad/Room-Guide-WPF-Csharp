222e88cfa6af5a438b8b0e127738a0c84146bb0d
Andrew Milko <an-mil@list.ru>
2017-06-18 12:00:35 +0300

    Room Guide WPF Csharp
    The application allows you to move from one room to another. Every room is unique, has its own decorations and special characteristics. There are temperature inf-on only in outside rooms and inf-on about arms and chairs only in inside rooms respectively. You can change those settings to understand how it works. Pay attention on the syntax: it’s the main part of this application. It takes time to understand what’s going on there though that application is quite simple.

---
+++ 7_Interfaces_Ex_9/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace _7_Interfaces_Ex_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.description = new System.Windows.Forms.Label();
+            this.goThroughTheDoor = new System.Windows.Forms.Button();
+            this.exits = new System.Windows.Forms.ComboBox();
+            this.goHere = new System.Windows.Forms.Button();
+            this.hide = new System.Windows.Forms.Button();
+            this.check = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // description
+            // 
+            this.description.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.description.Location = new System.Drawing.Point(13, 13);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(259, 110);
+            this.description.TabIndex = 0;
+            // 
+            // goThroughTheDoor
+            // 
+            this.goThroughTheDoor.Location = new System.Drawing.Point(12, 221);
+            this.goThroughTheDoor.Name = "goThroughTheDoor";
+            this.goThroughTheDoor.Size = new System.Drawing.Size(260, 23);
+            this.goThroughTheDoor.TabIndex = 1;
+            this.goThroughTheDoor.Text = "Go through the door";
+            this.goThroughTheDoor.UseVisualStyleBackColor = true;
+            this.goThroughTheDoor.Click += new System.EventHandler(this.goThroughTheDoor_Click);
+            // 
+            // exits
+            // 
+            this.exits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.exits.FormattingEnabled = true;
+            this.exits.Location = new System.Drawing.Point(147, 134);
+            this.exits.Name = "exits";
+            this.exits.Size = new System.Drawing.Size(125, 21);
+            this.exits.TabIndex = 2;
+            // 
+            // goHere
+            // 
+            this.goHere.Location = new System.Drawing.Point(12, 134);
+            this.goHere.Name = "goHere";
+            this.goHere.Size = new System.Drawing.Size(129, 23);
+            this.goHere.TabIndex = 3;
+            this.goHere.Text = "Go here:";
+            this.goHere.UseVisualStyleBackColor = true;
+            this.goHere.Click += new System.EventHandler(this.goHere_Click);
+            // 
+            // hide
+            // 
+            this.hide.Location = new System.Drawing.Point(12, 163);
+            this.hide.Name = "hide";
+            this.hide.Size = new System.Drawing.Size(260, 23);
+            this.hide.TabIndex = 4;
+            this.hide.Text = "Hide";
+            this.hide.UseVisualStyleBackColor = true;
+            this.hide.Click += new System.EventHandler(this.hide_Click);
+            // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(13, 192);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(259, 23);
+            this.check.TabIndex = 5;
+            this.check.Text = "Check";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 257);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.hide);
+            this.Controls.Add(this.goHere);
+            this.Controls.Add(this.exits);
+            this.Controls.Add(this.goThroughTheDoor);
+            this.Controls.Add(this.description);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Explore the House";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label description;
+        private System.Windows.Forms.Button goThroughTheDoor;
+        private System.Windows.Forms.ComboBox exits;
+        private System.Windows.Forms.Button goHere;
+        private System.Windows.Forms.Button hide;
+        private System.Windows.Forms.Button check;
+    }
+}
+
---
+++ 7_Interfaces_Ex_9/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7_Interfaces_Ex_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            CreateObjects();
+            MoveToANewLocation(garden);
+        }
+
+        Random random = new Random();
+
+        Location currentLocation;
+
+        Room dinningRoom;
+        RoomWithDoor livingRoom;
+        RoomWithDoor kitchen;
+
+        Outside garden;
+        OutsideWithDoor backYard;
+        OutsideWithDoor frontYard;
+
+        int MovesDone = 0;
+
+        int ChecksDone = 0;
+
+        public void CreateObjects()
+        {
+            dinningRoom = new Room("dinning room", "chrustal'naya lustra", "");
+            livingRoom = new RoomWithDoor("living room", "starinniy kover", "", "matalicheskay dver'");
+            kitchen = new RoomWithDoor("kitchen", "plita iz nerghaveushei stali", "", "setchataya dver'");
+
+            garden = new Outside("garden", true, "");
+            backYard = new OutsideWithDoor("back yard", false , "", "setchataya dver'");
+            frontYard = new OutsideWithDoor("front yard", false, "", "matalicheskay dver'");
+
+            dinningRoom.Exits = new Location[] { livingRoom, kitchen };
+            livingRoom.Exits = new Location[] { dinningRoom, frontYard };
+            kitchen.Exits = new Location[] { backYard, dinningRoom };
+
+            garden.Exits = new Location[] { frontYard, backYard };
+            frontYard.Exits = new Location[] { livingRoom, garden };
+            backYard.Exits = new Location[] { garden, kitchen };
+
+            livingRoom.DoorLocation = frontYard;
+            frontYard.DoorLocation = livingRoom;
+
+            kitchen.DoorLocation = backYard;
+            backYard.DoorLocation = kitchen; 
+        }
+
+        private void MoveToANewLocation(Location myLocation)
+        {  
+            MovesDone++;
+
+            currentLocation = myLocation;
+
+            exits.Items.Clear();
+
+            description.Text = currentLocation.Description;
+
+            for (int i = 0; i < currentLocation.Exits.Length; i++)
+            {
+                exits.Items.Add(currentLocation.Exits[i].name); 
+            }
+
+            exits.SelectedIndex = 0;
+
+            if (currentLocation is IHasExteriorDoor)
+            {
+                // goThroughTheDoor.Visible = true;
+                goThroughTheDoor.Enabled = true;
+
+            }
+            else
+            {
+                // goThroughTheDoor.Visible = false;
+                goThroughTheDoor.Enabled = false;
+            }
+        }
+
+        private void goHere_Click(object sender, EventArgs e)
+        {
+            MoveToANewLocation(currentLocation.Exits[exits.SelectedIndex]);
+        }
+
+        private void goThroughTheDoor_Click(object sender, EventArgs e)
+        {
+            IHasExteriorDoor hasDoor = currentLocation as IHasExteriorDoor;
+            MoveToANewLocation(hasDoor.DoorLocation);
+        }
+
+        private void hide_Click(object sender, EventArgs e)
+        {
+            Random myRandom = new Random();
+
+            int randomNumber = myRandom.Next(1, 7);
+
+            description.Text += " Your random number is " + randomNumber + ". You can start to go in search!";
+
+            if (randomNumber == 1)
+            {
+                // frontyard +
+                frontYard.opponent = " I'm here.";
+                             
+            }
+            else if (randomNumber == 2)
+            {
+                // garden
+                garden.opponent = " I'm here.";
+            }
+            else if (randomNumber == 3)
+            {
+                // backyard +
+                backYard.opponent = " I'm here.";
+            }
+            else if (randomNumber == 4)
+            {
+                // livingroom +
+                livingRoom.opponent = " I'm here.";
+            }
+            else if (randomNumber == 5)
+            {
+                // dinningroom
+                dinningRoom.opponent = " I'm here.";
+            }
+            else if (randomNumber == 6)
+            {
+                // kitchen +
+                kitchen.opponent = " I'm here.";
+            }
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+            ChecksDone++;
+
+            if(String.IsNullOrEmpty(currentLocation.opponent) != true)
+            {
+                description.Text += currentLocation.opponent + " You have moved " + MovesDone + " times and made " + ChecksDone + " attempts";
+            }
+            else
+            {
+                description.Text += " Here's empty, check another room!";
+            }
+        }
+    }
+
+    abstract class Location
+    {
+        public Location(string name, string opponent)
+        {
+            this.name = name;
+            this.opponent = opponent;
+        }
+
+        public string name { get; private set; }
+
+        public string opponent { get; set; }
+
+        public Location[] Exits;
+
+        public virtual string Description
+        {
+            get
+            {
+                string description = "You are located in " + name + ". You also can see doors, which lead to:";
+                for (int i = 0; i < Exits.Length; i++)
+                {
+                    description += " " + Exits[i].name;
+                    if (i != Exits.Length - 1)
+                    {
+                        description += ",";
+                    }
+                }
+                description += ".";
+                return description;
+            }
+        }
+    }
+
+    class Room : Location
+    {
+        public Room(string name, string decoration, string opponent) : base(name, opponent)
+        {
+            this.decoration = decoration;
+        }
+
+        private string decoration;
+
+        public override string Description
+        {
+            get
+            {
+                return base.Description + " Here you can see " + decoration + ".";
+            }
+        }
+    }
+
+    class Outside : Location
+    {
+        public Outside(string name, bool hot, string opponent) : base(name, opponent)
+        {
+            this.hot = hot;
+        }
+
+        public bool hot;
+
+        public override string Description
+        {
+            get
+            {
+                string newDescription = base.Description;
+                if (hot)
+                {
+                    newDescription += " Here's hot.";
+                }           
+                return newDescription;
+            }
+        }
+    }
+
+    interface IHasExteriorDoor
+    {
+        string DoorDescription { get; }
+
+        Location DoorLocation { get; set; }
+    }
+
+    class RoomWithDoor : Room, IHasExteriorDoor
+    {
+        public RoomWithDoor(string name, string decoration, string opponent, string doorDescription) : base(name, decoration, opponent)
+        {
+            doorDescription = DoorDescription;
+        }
+
+        public string DoorDescription { get; set; }
+
+        public Location DoorLocation { get; set; }
+    }
+
+    class OutsideWithDoor : Outside, IHasExteriorDoor
+    {
+        public OutsideWithDoor(string name, bool hot, string opponent,  string doorDescription) : base(name, hot, opponent)
+        {
+            doorDescription = DoorDescription;
+        }
+
+        public string DoorDescription { get; set; }
+
+        public Location DoorLocation { get; set; }
+    }
+
+    /*
+       1) создание класса Opponent *
+       
+            *   закрытое поле Location(myLocation) - отслеживание положения
+                закрытое поле Random(random) ** - поиск случайного укромного места 
+              
+            **  Random myRandom = new Random();
+                int randomNumber = myRandom.Next(1, 11);
+         
+       2) создание интерфейса (который описывает укромные места)
+       3) сделать изменения в форме (возможность проверять наличие укромных местечек и отслеживать, сколько ходов было сделано, пытаясь найти соперника) 
+       4) добавление метода Move() - перемещение соперника
+       5) добавление метода Check() с параметром location - возвращение значение true, когда соперник прячется
+   */  
+}
